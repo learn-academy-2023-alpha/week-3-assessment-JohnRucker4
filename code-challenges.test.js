ad406c3ac96f9d5ddc23aa8eaa566bbea2884190
johnrucker4 <jrucker4@outlook.com>
2023-03-05 23:51:11 -0800

    completed week 3 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,67 +1,148 @@
-// ASSESSMENT 3: Coding Practical Questions with Jest
+// // ASSESSMENT 3: Coding Practical Questions with Jest
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
+// // Please read all questions thoroughly
+// // Pseudo coding is REQUIRED
+// // If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
+// // Use test driven development to complete the following questions
+// // Add appropriate dependencies: $ yarn add jest
 
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// // Reminder: The test will call your function
+// // Run the file with the following command: $ yarn jest
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
+// Input: a number where number >= 2
+// Output: recreate the fibonacci sequence in an array
+// Remember, Fibonacci = the 3rd, 4th, 5th...numbers are equal to the sum of the two previous numbers
+// Describe "fibArry"
+describe("fibArray", () => {
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
-
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+  it("returns an array that length containing the numbers of the Fibonacci sequence", () => {
+      expect(fibArray(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+      expect(fibArray(fibLength2)).toEqual([
+      1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+      ])
+    })
+  })
 
-// b) Create the function that makes the test pass.
+// //   Test Suites: 1 failed, 1 total
+// //   Tests:       1 failed, 1 total
+// //   Snapshots:   0 total
+// //   Time:        0.133 s, estimated 1 s
 
-// --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
-// Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+//   // // b) Create the function that makes the test pass.
 
-// a) Create a test with expect statements for each of the variables provided.
+// Create fibArray function
+const fibArray = (fiboLength) => {
+// Create array using first two numbers of Fibonacci sequence
+    let theFib = [1, 1]
+// Create for loop at zero index 2, i keeps going where its less than fiboLength
+    for (i = 2; i < fiboLength; i++) {
+// The index should be the sum of the previous two numbers in fibArray
+      theFib.push(theFib[i - 2] + theFib[i - 1])
+    }
+      return theFib
+  }
 
+// // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
+// // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+// Input: object with 7 key:value pairs
+// Output: Array of numbers, sorted from least to greatest
+// Describe "studyUp"
+describe("studyU", () => {
 const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
+    sunday: 90,
+    monday: 30,
+    tuesday: 20,
+    wednesday: 15,
+    thursday: 30,
+    friday: 15,
+    saturday: 60,
+  }
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
-
 const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
+    sunday: 100,
+    monday: 10,
+    tuesday: 45,
+    wednesday: 60,
+    thursday: 20,
+    friday: 15,
+    saturday: 65,
+  }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
+  it("returns an array of the values sorted from least to greatest", () => {
+    expect(studyUp(studyMinutesWeek1)).toEqual([
+    15, 15, 20, 30, 30, 60, 90,
+    ])
+    expect(studyUp(studyMinutesWeek2)).toEqual([
+    10, 15, 20, 45, 60, 65, 100,
+    ])
+  })
+})
 
-// b) Create the function that makes the test pass.
+// // Test Suites: 1 failed, 1 total
+// // Tests:       1 failed, 1 total
+// // Snapshots:   0 total
+// // Time:        0.15 s, estimated 1 s
+// // // b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+// Create studyUp function
+const studyUp = (studyMin) => {
+// Create array with values from studyMin
+    let studyTime = Object.values(studyMin)
+// Use .sort to sort array from least to greatest
+    return Object.values(studyMin).sort((a, b) => a - b)
+  }
 
-// a) Create a test with expect statements for each of the variables provided.
+// // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// // a) Create a test with expect statements for each of the variables provided.
+
+// Input: transaction numbers in an array
+// Output: array that keeps track of transactions in an array
+// Describe "accountView"
+describe("accountView", () => {
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
-
 const accountTransactions2 = [250, -89, 100, -96]
 // Expected output: [250, 161, 261, 165]
-
 const accountTransactions3 = []
 // Expected output: []
+it("returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    expect(accountView(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(accountView(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(accountView(accountTransactions3)).toEqual([])
+  })
+})
 
-// b) Create the function that makes the test pass.
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.134 s, estimated 1 s
+// // b) Create the function that makes the test pass.
+
+// Create accountView function
+const accountView = (transactions) => {
+// Create empty array for the account balance
+    let acctBalance = []
+// Use .forEach to iterate through the transactions array that passes value and index
+    transactions.forEach((v, i) => {
+// Use .push for value at zero-index, when index isn't at zero-index, push value of index into acctBalance
+       i === 0 ? acctBalance.push(v) : acctBalance.push(v + acctBalance[i - 1])
+    })
+    return acctBalance
+  }
+
+//   Test Suites: 1 passed, 1 total
+//   Tests:       3 passed, 3 total
+//   Snapshots:   0 total
+//   Time:        0.129 s, estimated 1 s
--- interview-questions.md
+++ interview-questions.md
@@ -9,47 +9,61 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
+JSX means javascript extensible markup. It allows JS and HTML to work together.  One syntax difference is the need for curly braces when pushing any functions from JS to HTML.
 
 Researched answer:
 
-1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
+By allowing JS and HTML to work together via JSX, we can make create and/or make edits to the webpage with dynamic JS code straight to HTML.  In order to facilitate such transition, JS functions must be passed through curly braces.
+
+2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
 Your answer:
+Yarn, Yet Another Resource Negotiator, is a package manager. It is used for managing the dependencies in a project. The package.json and yarn.lock files can be modified when you run command yarn in the terminal.
 
 Researched answer:
 
+Apache Hadoop was a framework that processed datasets across many computers.  Yarn, an acronym for Yet Another Resource Negotiator, is the second generation version of Hadoop. A package manager, Yarn can keep track of various versions of code. 
 3. What is a React component?
 
 Your answer:
+A React component is resuable code that can update changes to the webpage without refreshing the entire page.
 
 Researched answer:
+React can break down portions of code for a webpage and sequester them into reusable blocks. These are called React components.  They can be used similarly to how functions work in JS. When called on, they can provide updates to the webpage without refreshin the entire page.
 
 4. What is the difference between state values and props in React?
 
 Your answer:
+Props, short for properties, pass data from a parent component to a child component.  State values are used to store the state of data when a user interacts with a webpage.  
 
 Researched answer:
+As a user interacts with a webpage, the data may need to be stored.  React uses objects in JS called States and props.  The state manages the current data gathered from user interaction. When the state is modified, it is reflected on the webpage. Passing data into the components that facilitate the change is are called Properties, or props for short.
 
 5. What is the DOM?
 
 Your answer:
+DOM stands for Document Object Model. Its what is displayed when there is user interaction taking place on a webpage.
 
 Researched answer:
+DOM stands for Document Object Model. The DOM, through JS, allows user interaction on an webpage.  The design structure involves "tags" that hold information under umbrellas. The head tag is first, then under that umbrella comes the body tag. Webpage aesthetic and intended interaction points are then coded out.
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
 Your answer:
+A div tag acts as a container for code in HTML. Information on a span tag escapes me at the moment, but I can find out and give you an answer.
 
 Researched answer:
+A span tag is a container inside of a parent tag that allows styling for its contents.  The div tag is also a container that sections off blocks of code that extends to the end of the webpage. 
 
 ## Looking Ahead: Terms for Next Week
 
 1. Object-oriented programming:
-
+In order to maintain the growing complexities of applications, programmers created containers of data to work in without affecting the entire program.
 2. Ruby:
-
+Ruby is an object-oriented language.
 3. Implicit return:
-
+If there is no return specified value of the last line of a method is automatically returned.
 4. Ruby blocks:
-
+An anonymous function that is passed to a method to determine the outcome.
 5. Ruby hashes:
+A is a dictionary-like container for unique keys and their corresponding values.
